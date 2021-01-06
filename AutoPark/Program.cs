28969e2f57012f19830648e3b8ace3985610fbcf
STALINovsky <62829813+STALINovsky@users.noreply.github.com>
2021-01-06 18:56:32 +0300

    Complete 1 Level

--- AutoPark/Program.cs
+++ AutoPark/Program.cs
@@ -1,12 +1,44 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
 
 namespace AutoPark
 {
-    class Program
+    static class Program
     {
+        static List<VehicleType> CreateVehicleTypes()
+        {
+            return new List<VehicleType>()
+            {
+                new ("Bus", 1.2m),
+                new ("Car", 1m),
+                new ("Rink", 1.5m),
+                new ("Tractor", 1.2m),
+            };
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            var vehicleTypes = CreateVehicleTypes();
+            foreach (var vehicleType in vehicleTypes)
+            {
+                vehicleType.Display();
+            }
+
+            var lastVehicleType = vehicleTypes.Last();
+            lastVehicleType.TaxCoefficient = 1.3m;
+
+            decimal CoefficientSelector(VehicleType vehicleType) => vehicleType.TaxCoefficient;
+            var maxTaxCoefficient = vehicleTypes.Max(CoefficientSelector);
+            var averageCoefficient = vehicleTypes.Average(CoefficientSelector);
+
+            foreach (var vehicleType in vehicleTypes)
+            {
+                Console.WriteLine(vehicleType);
+            }
+
         }
     }
 }
---
+++ AutoPark/VehicleType.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace AutoPark
+{
+    public class VehicleType
+    {
+        public string TypeName { get; set; }
+        public decimal TaxCoefficient { get; set; }
+
+        public VehicleType()
+        {
+            
+        }
+
+        public VehicleType(string typeName, decimal taxCoefficient)
+        {
+            TypeName = typeName;
+            TaxCoefficient = taxCoefficient;
+        }
+
+        public void Display() => Console.WriteLine(ToString());
+        public override string ToString() => $"Type name: {TypeName}, Tax coefficient: {TaxCoefficient}";
+    }
+}
