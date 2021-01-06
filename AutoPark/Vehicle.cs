8f318a10f444e42a81ca59fb471f12c8938ab067
STALINovsky <62829813+STALINovsky@users.noreply.github.com>
2021-01-06 19:07:41 +0300

    Fix ToString method

---
+++ AutoPark/Vehicle.cs
@@ -0,0 +1,17 @@
+﻿namespace AutoPark
+{
+    public class Vehicle
+    {
+        public VehicleType VehicleType { get; set; }
+        public string ModelName { get; set; }
+        public string RegistrationNumber { get; set; }
+        /// <summary>
+        /// Weight in KG
+        /// </summary>
+        public int Weight { get; set; }
+
+        public int ManufactureYear { get; set; }
+        public int Mileage { get; set; }
+
+    }
+}
