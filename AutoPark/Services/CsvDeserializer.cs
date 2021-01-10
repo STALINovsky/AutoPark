b8afe64f20080c465af8072937b6e90b9b65fa90
STALINovsky <62829813+STALINovsky@users.noreply.github.com>
2021-01-10 13:14:47 +0300

    Complete services

---
+++ AutoPark/Services/CsvDeserializer.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using AutoPark.Model;
+using AutoPark.Model.Vehicles;
+using AutoPark.Model.Vehicles.Engines;
+
+namespace AutoPark.Services
+{
+    public static class CsvDeserializer
+    {
+        private const char FIELD_SEPARATOR = ',';
+
+        private static List<string> GetFieldsList(string cvsString)
+        {
+           return cvsString.Split(FIELD_SEPARATOR).ToList();
+        }
+
+        public static Vehicle DeserializeVehicle(string csvString, IList<VehicleType> vehicleTypes, IEnumerable<Rent> rents)
+        {
+            var fields = GetFieldsList(csvString);
+            var vehicle =  new Vehicle()
+            {
+                Id = int.Parse(fields[0]),
+                VehicleType = vehicleTypes.First(type => type.Id == int.Parse(fields[1])),
+                ModelName = fields[2],
+
+                RegistrationNumber = fields[3],
+                Weight = int.Parse(fields[4]),
+                ManufactureYear = int.Parse(fields[5]),
+
+                Mileage = int.Parse(fields[6]),
+                Color = Enum.Parse<CarColor>(fields[7]),
+                AbstractEngine = fields[8] switch
+                {
+                    EngineTypesNamesConstant.GasolineEngine =>
+                        new GasolineEngine(double.Parse(fields[9]), double.Parse(fields[10]), int.Parse(fields[11])),
+
+                    EngineTypesNamesConstant.DieselEngine =>
+                        new DieselEngine(double.Parse(fields[9]), double.Parse(fields[10]), int.Parse(fields[11])),
+
+                    EngineTypesNamesConstant.ElectricalEngine =>
+                        new ElectricalEngine(double.Parse(fields[9]), double.Parse(fields[11])),
+
+                    _ => throw new ArgumentOutOfRangeException()
+                },
+            };
+            vehicle.Rents = rents.Where(rent => rent.VehicleId == vehicle.Id).ToList();
+
+            return vehicle;
+        }
+
+        public static VehicleType DeserializeVehicleType(string csvString)
+        {
+            var fields = GetFieldsList(csvString);
+            return new VehicleType(int.Parse(fields[0]), fields[1], decimal.Parse(fields[2]));
+        }
+
+        public static Rent DeserializeRent(string csvString)
+        {
+            var fields = GetFieldsList(csvString);
+            return new Rent(int.Parse(fields[0]), DateTime.Parse(fields[1]), decimal.Parse(fields[2]));
+        }
+
+        public static List<string> DeserializeOrders(string csvString)
+        {
+            return GetFieldsList(csvString);
+        }
+        
+    }
+}
--- AutoPark/Services/OutputService.cs
+++ AutoPark/Services/OutputService.cs
@@ -1,8 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using AutoPark.Model.Vehicles;
 
-namespace AutoPark
+namespace AutoPark.Services
 {
     public static class OutputService
     {
@@ -14,16 +15,19 @@ namespace AutoPark
             }
         }
 
-        public static void PrintSameElements<T>(IEnumerable<T> elements)
+        public static void PrintVehicleTable(IEnumerable<Vehicle> vehicles)
         {
-            var recurringItems = elements.GroupBy(x => x)
-                .Where(g => g.Count() > 1)
-                .Select(y => new { Value = y.Key, Count = y.Count() });
+            Console.WriteLine($"{"Id",-5}{"Type",-10}{"ModelName",-25}{"Number",-15}" +
+                              $"{"Weight (kg)",-15}{"Year",-10}{"Mileage",-10}{"Color",-10}" +
+                              $"{"Income",-10}{"Tax",-10}{"Profit",-10}");
 
-
-            foreach (var recurringItem in recurringItems)
+            foreach (var item in vehicles)
             {
-                Console.WriteLine($"vehicle: {recurringItem.Value}, count: {recurringItem.Count}");
+                Console.WriteLine(
+                    $"{item.Id,-5}{item.VehicleType.TypeName,-10}{item.ModelName,-25}" +
+                    $"{item.RegistrationNumber,-15}{item.Weight,-15}{item.ManufactureYear,-10}" +
+                    $"{item.Mileage,-10}{item.Color,-10}{item.TotalIncome,-10:0.00}" +
+                    $"{item.TaxPerMonth,-10:0.00}{item.TotalProfit,-10:0.00}");
             }
         }
     }
